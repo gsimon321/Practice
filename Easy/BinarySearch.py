fe4b4124b221d0f03302e79e96172ec56e9fda82
GaborSimon <60612435+gsimon321@users.noreply.github.com>
2022-07-22 18:03:54 -0400

    few more

--- Easy/BinarySearch.py
+++ Easy/BinarySearch.py
@@ -1,26 +1,13 @@
-from ast import List
-from email.errors import FirstHeaderLineIsContinuationDefect
-
-from jmespath import search
-
-# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
-# If target exists, then return its index. Otherwise, return -1.
-# You must write an algorithm with O(log n) runtime complexity.
-
-import numpy as np
 
 class Solution:
         def search(self, nums: List[int], target: int) -> int:
-            k = len(nums)//2;
-            if(nums[k] == target):
-                return nums[k]
-            elif(nums[k]<< target):
-                secondHalf = np.array_split(nums,2)[1]
-                search(secondHalf,target)
-            else:
-                firstHalf = np.array_split(nums,2)[0]
-                search(firstHalf,target) 
-
-                 
-
-
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                mid = (l+r)//2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return -1
