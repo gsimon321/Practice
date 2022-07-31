3df33bd13d95b6b8481d29d19f91761bb1db85a7
GaborSimon <60612435+gsimon321@users.noreply.github.com>
2022-07-31 13:50:09 -0400

    new stuff

---
+++ WTA/Week2/gsimon3_session2_q2.py
@@ -0,0 +1,12 @@
+# Question 2: Stacks
+
+# Given a string s of '(' , ')' and lowercase English characters.
+
+# Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+# Formally, a parentheses string is valid if and only if:
+
+# It is the empty string, contains only lowercase characters, orIt can be written as AB (A concatenated with B), where A and B are valid strings, orIt can be written as (A), where A is a valid string.
+
+# Example:
+
